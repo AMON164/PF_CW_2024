1bf8d4a31fc3eeaba5201303dd06cc1283a9ede1
AMON164 <amutuhaireamon5@gmail.com>
2024-08-31 11:04:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hi! welcome to the ATM machine\n" << endl;
+    cout << "please select an option from the menu below:\n" << endl;
+
+    // options to the user
+
+    cout << "l->Login" << endl;
+    cout << "c->Create New Account" << endl;
+    cout << "q->Quit" << endl;
+
+
+    return 0;
+}
